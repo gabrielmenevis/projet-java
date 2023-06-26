2eac2366719031ec7fff46b3f17f383a52b8d917
Poornima <137071426+htr11@users.noreply.github.com>
2023-06-26 10:45:40 +0200

    Update Personnage.java

---
+++ Personnage.java
@@ -0,0 +1,17 @@
+public class Personnage {
+
+    private String nom;
+    private int pv = 0;
+    
+    public Personnage(String nom, int pv) {
+        this.nom = nom;
+        this.pv = pv;
+    }
+
+
+
+
+
+
+
+}
