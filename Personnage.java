175160048ea8ac48371a28746003758c89584853
Lucas <l.leroux12@gmail.com>
2023-06-26 11:59:40 +0200

    removed changes

--- Personnage.java
+++ Personnage.java
@@ -3,7 +3,7 @@ public class Personnage {
     private String nom;
     private int pv = 0;
     
-    public Personnage(String nom, int pv, int pc) {
+    public Personnage(String nom, int pv) {
         this.nom = nom;
         this.pv = pv;
     }
