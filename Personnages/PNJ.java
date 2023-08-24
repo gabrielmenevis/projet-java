8154f3c9f897fec9dd06dff390919b1d824aafd2
Poornima <137071426+htr11@users.noreply.github.com>
2023-08-24 19:26:32 +0200

    Objet + Quetes

--- Personnages/PNJ.java
+++ Personnages/PNJ.java
@@ -1,7 +1,28 @@
 package Personnages;
 
+<<<<<<< Updated upstream
 public class PNJ {
 
     
     
+=======
+public class PNJ extends Personnage {
+    private String indice;
+    
+    public PNJ(String nom, String indice) {        
+        super(nom, 0, 0, 0, 0);
+        this.indice = indice;
+    }
+
+    public String getIndice() {
+        return indice;
+    }
+
+    public void presentation() {
+        System.out.println("");
+        
+    }
+    
+
+>>>>>>> Stashed changes
 }
