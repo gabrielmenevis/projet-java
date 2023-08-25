455b0925c60a10d333330dde948cbd2b0f266a28
Lucas <l.leroux12@gmail.com>
2023-08-25 02:23:38 +0200

    Adaptation du combat à la classe PNJ

--- Personnages/PNJ.java
+++ Personnages/PNJ.java
@@ -3,12 +3,16 @@ package Personnages;
 public class PNJ extends Personnage {
     private String indice;
     private String texte_pnj;
-    
-    public PNJ(String nom, String indice, String texte_pnj) {        
-        super(nom, 0, 0, 0, 0);
+    private int base_p_attaque;
+
+    public PNJ(String nom, String indice, String texte_pnj, int max_pv, int p_attaque, int p_charisme) {
+        super(nom, max_pv, max_pv, p_attaque, p_charisme);  // Ici, max_pv est passé deux fois à cause du constructeur de Personnage
         this.indice = indice;
         this.texte_pnj = texte_pnj;
+        this.base_p_attaque = p_attaque;
     }
+
+
 
     public String getIndice() {
         return indice;
@@ -19,28 +23,32 @@ public class PNJ extends Personnage {
     }
 
     public void presentation() {
-        System.out.println("Je suis " + getNom() + getTextePNJ());
+        System.out.println("Je suis " + getNom() + ". " + getTextePNJ());
     }
 
     public static void main(String[] args) {
 
-        PNJ pnj1 = new PNJ("Margerite la cuisinière. ", "à définir", "La cuisine est fermée au public, si vous voulez y entrer, il faudra me passer sur le corps");
+        PNJ pnj1 = new PNJ("Margerite la cuisinière. ", "à définir", "La cuisine est fermée au public, si vous voulez y entrer, il faudra me passer sur le corps", 100, 120, 34);
         pnj1.presentation();
 
-        PNJ pnj2 = new PNJ("Hervé le plongeur. ", "à définir", "idk yet");
+        PNJ pnj2 = new PNJ("Hervé le plongeur. ", "à définir", "idk yet", 12, 12, 12);
         pnj2.presentation();
 
-        PNJ pnj3 = new PNJ("le Chien méchant.", "à définir", "Ouaf Ouaf");
+        PNJ pnj3 = new PNJ("le Chien méchant.", "à définir", "Ouaf Ouaf", 12, 12, 12);
         pnj3.presentation();
 
-        PNJ pnj4 = new PNJ("l'homme nu du hammam? ", " Pas d'indice pour vous..", "Quelle est votre réponse?");
+        PNJ pnj4 = new PNJ("l'homme nu du hammam? ", " Pas d'indice pour vous..", "Quelle est votre réponse?", 12, 12, 12);
         pnj4.presentation();
 
-        PNJ pnj5 = new PNJ("Moe. ", "à définir", "Une bière?");
+        PNJ pnj5 = new PNJ("Moe. ", "à définir", "Une bière?", 12, 12, 12);
         pnj5.presentation();
 
-        PNJ pnj6 = new PNJ("Igor."," idk", " idk");
+        PNJ pnj6 = new PNJ("Igor."," idk", " idk", 12, 12, 12);
         pnj6.presentation();
 
+    }
+
+    public void resetPAttaque(){
+        this.p_attaque = this.base_p_attaque;
     }
 }
--- testcombat.java
+++ testcombat.java
@@ -1,18 +1,20 @@
 
+import Personnages.PNJ;
+
 import java.io.IOException;
 
 public class testcombat {
     public static void main(String[] args) throws IOException {
         // Création de deux personnages
-        Personnages.Artiste artiste = new Personnages.Artiste("Leonardo", 100, 100, 20, 15);
-        Personnages.Courgette courgette = new Personnages.Courgette("Cory", 80, 80, 25, 10);
+        Personnages.Artiste artiste = new Personnages.Artiste("Leonardo", 100, 100, 20, 40);
+        PNJ Florian = new PNJ("Florian","pompiste", "Je suis délégué MIMO", 100, 10, 40);
 
         // Présentation des personnages
         artiste.presentation();
-        courgette.presentation();
+        Florian.presentation();
 
         // Création du combat
-        combats.combats combat = new combats.combats(artiste, courgette);
+        combats.combats combat = new combats.combats(artiste, Florian);
 
         // Lancement du combat
         combat.lancerCombat();
