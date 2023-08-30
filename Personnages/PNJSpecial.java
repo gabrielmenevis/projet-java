903a658e4dab9c5ea5fcf1de851683ab8b89274d
gabrielmenevis <128372786+gabrielmenevis@users.noreply.github.com>
2023-08-30 12:36:46 +0200

    javadoc PNJ et PNJSpecial

--- Personnages/PNJSpecial.java
+++ Personnages/PNJSpecial.java
@@ -6,6 +6,14 @@ import java.util.Scanner;
 import Combats.Combat;
 import Objets.Objet;
 
+/**
+ * Représentation d'un PNJ spécial dans le jeu. Hérite de PNJ. À la différence des PNJ non spéciaux, le PNJ
+ * spécial n'est pas aléatoire. Certains menus d'interaction et certains comportements diffèrent.
+ * En particulier, le PNJ spécial possède un attribut qui indique le nom de l'objet demandé, un autre qui indique
+ * s'il est satisfait (il a obtenu l'objet demandé) et un indice à afficher au joueur quand il est vaincu ou
+ * satisfait.
+ * L'indicateur isSpecial est vrai pour cette classe.
+ */
 public class PNJSpecial extends PNJ {
 
     private String indice;
@@ -13,6 +21,19 @@ public class PNJSpecial extends PNJ {
     private boolean satisfait;
     private boolean isSpecial = true;
 
+    /**
+     * Constructeur de PNJSpecial. Prend les paramètres à passer au constructeur de PNJ, plus l'indice,
+     * le nom de l'objet demandé. Initialise satisfait à false.
+     * @param nom : le nom du PNJ
+     * @param type : le type du PNJ
+     * @param article : l'article à accoler au type
+     * @param indice : l'indice à afficher au joueur
+     * @param textePNJ : la réplique du PNJ
+     * @param max_pv : le nombre maximum de PV
+     * @param p_attaque : le nombre de PA
+     * @param p_charisme : le nombre de PC
+     * @param nomObjetDemande : le nom de l'objet voulu par le PNJ
+     */
     public PNJSpecial(String nom, String type, String article, String indice, String textePNJ, int max_pv, int p_attaque, int p_charisme, String nomObjetDemande) {
         super(nom, type, article, textePNJ, max_pv, p_attaque, p_charisme);  // Ici, max_pv est passé deux fois à cause du constructeur de Personnage
         this.indice = indice;
@@ -43,11 +64,18 @@ public class PNJSpecial extends PNJ {
     public void setSatisfait(boolean satisfait){
         this.satisfait = satisfait;
     }
+
     public boolean isSpecial(){
         return this.isSpecial;
     }
 
-
+    /**
+     * Menu d'interaction avec le PNJ et choix du joueur. Surcharge la méthode de la classe parente pour définir
+     * un comportement particulier si le PNJ spécial est vaincu ou satisfait. Dans ce cas, le PNJ spécial 
+     * n'est plus agressif avec le joueur et celui-ci peut lui demander de répéter l'indice.
+     * @return le choix du joueur ("1" pour parler/répéter l'indice, "2" pour donner un objet, "3" pour se 
+     * battre, "4" pour s'en aller)
+     */
     public String menuPNJ(){
 
         String choix = "";
@@ -74,6 +102,11 @@ public class PNJSpecial extends PNJ {
         return choix;
     }
 
+    /**
+     * Surcharge la méthode de la classe parente pour implémenter un comportement particulier si le PNJ spécial est 
+     * satisfait ou vaincu. Dans ce cas, il donne son indice. Sinon, il appelle la méthode de la classe parente.
+     * @return true si le joueur a résolu l'énigme (seulement possible avec Boss), sinon false
+     */
     public boolean parler(){
         if(!this.satisfait && !this.getVaincu()){ // si ni satisfait ni vaincu, répète sa phrase
             super.parler();
@@ -84,6 +117,13 @@ public class PNJSpecial extends PNJ {
         return false;
     }
 
+    /**
+     * Lancement du combat avec le PNJ spécial. Surcharge la méthode de la classe parente pour afficher des 
+     * messages personnalisés si le joueur a déjà vaincu ou satisfait le PNJ. Sinon, le combat se lance.
+     * @param perso : le joueur que le PNJ doit combattre
+     * @return true si le joueur meurt au combat, sinon false
+     * @throws IOException : erreur lecture de fichier dans Combat.lancerCombat()
+     */
     public boolean combattre(Personnage perso) throws IOException{
 
         boolean mourir;
@@ -113,16 +153,23 @@ public class PNJSpecial extends PNJ {
         return mourir;
     }
 
+    /**
+     * Détermine si le PNJ spécial accepte un objet donné. Le PNJ spécial n'accepte que l'objet dont le nom
+     * est égal à son attribut nomObjetDemande
+     * @param o : objet donné
+     * @return true si le PNJ accepte l'objet, sinon false
+     */
     public boolean recevoirObjet(Objet o){
         
         boolean accepte;
 
+        // si l'objet est l'objet demandé
         if(o.getNom().equals(this.nomObjetDemande)){ // TODO: revoir messages
             accepte = true;
-            this.satisfait = true;
+            this.satisfait = true; // le PNJ est maintenant satisfait
             System.out.println();
             System.out.println(o.getArticleIndefini() + " " + o.getNom() + " !!!!! J'en ai toujours rêvé...");
-            this.donnerIndice();
+            this.donnerIndice(); // le PNJ donne l'indice
             System.out.println();
         }
 
@@ -137,7 +184,9 @@ public class PNJSpecial extends PNJ {
         return accepte;
     }
 
-
+    /**
+     * Le PNJ spécial donne son indice au joueur.
+     */
     public void donnerIndice(){
         System.out.println();
         System.out.println(this.decrire() + " vous dit :");
